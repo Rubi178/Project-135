8ba4e901bb4d5413c8873168c9326926fdf96013
Rubi178 <78242301+Rubi178@users.noreply.github.com>
2021-11-29 18:44:12 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+status = "";
+video = "";
+
+function preload() {
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHtml = "status: detecting objects";
+}
+function modelLoaded() {
+    console.log("Model is loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
